b82a6f6153c3d7ce6d599cbd2c666af7bcbc074f
gael.sola <gaelsola@hotmail.fr>
2021-08-23 18:10:33 +0700

    done wrapper functions around lcvplants::LCVP() and WorldFlora::WFO.match()

---
+++ R/solve_lcvp.R
@@ -0,0 +1,109 @@
+
+## Function to run a Taxonomic Name Resolution algorithm and output result harmonized with:
+## 1. The whole input list of species is returned
+## 2. Matches are returned with species and authors separated
+## 3. Taxonomic status in lower case
+## 4. Score or fuzzy matching indicator
+
+## --- LCVP -----------------------------------------------------------------
+## --- Offline
+## --- Data source: LCVP::tab_lcvp
+## --- Algorithm: lcvplants::LCVP()
+## --- Performs better with full list than slices. Multi-cores integrated. 
+## --- Recommended params: max.distance = 2 
+## ---
+## --- https://idiv-biodiversity.github.io/lcvplants/articles/taxonomic_resolution_using_lcplants.html#running-lcvplants
+## ---
+## --- function params: 
+## ---    .data: vector of taxonomic names with or without authors. Genus are not evaluated if submitted alone. 
+## ---           Preferably output of species_clean().
+## ---    .save_table: NULL or path to export the results. if .path exists (function embedded 
+## ---                 in a higher level function call) it is used in the file name 
+
+solve_lcvp <- function(.taxon, .infrasp_abb = c("subsp.", "ssp.", "var.", "subvar.", "f.", "subf.", "forma"), .save_table = NULL){
+  
+  # ## !!! For testing only
+  # .path    <- "demo/NFMA_species_mess.csv"
+  # .taxon <- .path %>% species_clean() %>%
+  #   filter(!is.na(input_ready)) %>% 
+  #   pull(input_ready) %>% 
+  #   unique()
+  # .infrasp_abb <- c("subsp.", "ssp.", "var.", "subvar.", "f.", "subf.", "forma")
+  # .save_table <- path_res
+  # ## !!! 
+  
+  ## Check function inputs
+  stopifnot("LCVP" %in% installed.packages())
+  stopifnot("lcvplants" %in% installed.packages())
+  if (!is.null(.save_table)) stopifnot(dir.exists(.save_table))
+  
+  
+  ## Remove genus alone if in the data
+  input <- setdiff(.taxon, word(.taxon)) %>% unique() %>% sort()
+  
+  ## Find table name if .path exists
+  filename <- if_else(exists(".path"), get_filename(.path), "")
+  
+  ## --- RUN LCVP ---
+  message("...Running Leipzig Catalogue of Vascular Plants.")
+  time1 <- Sys.time()
+  
+  solved_lcvp <- lcvplants::LCVP(input, max.distance = 2, genus_search = T)
+  
+  time2 <- Sys.time()
+  dt    <- round(as.numeric(time2-time1, units = "secs"))
+  message(paste0("...Taxons solved with LCVP", " - ", dt, " sec."))
+  ## --- END RUN LCVP ---
+  
+  # table(solved_lcvp$Status, useNA = "always")
+
+  
+  ## --- Harmonize ---
+  solved_out <- tibble(submitted_name = .taxon) %>%
+    left_join(solved_lcvp, by = c("submitted_name" = "Submitted_Name")) %>%
+    mutate(
+      fuzzy_dist    = Insertion + Deletion + Substitution,
+      fuzzy         = if_else( fuzzy_dist > 0, TRUE, FALSE),
+      status        = if_else(Status == "", "noref", Status),
+      accepted_id   = NA_character_,
+      refdata_id    = "lcvp",
+      refdata       = "Leipzig Catalogue of Vascular Plants",
+      matching_algo = "lcvplants::LCVP()",
+      
+      ## Split names based on space
+      split_input  = LCVP_Accepted_Taxon %>% str_split(" ", n = 5),
+      genus        = map_chr(split_input, 1, .default = ""),
+      epithet      = map_chr(split_input, 2, .default = ""),
+      infrasp      = map_chr(split_input, 3, .default = ""),
+      infrasp_name = map_chr(split_input, 4, .default = ""),
+      leftover     = map_chr(split_input, 5, .default = ""),
+      
+      ## Separate name from authors (!!! Doesn't handle sections, too rare)
+      accepted_name = if_else(
+        infrasp %in% .infrasp_abb,
+        paste(genus, epithet, infrasp, infrasp_name, sep = " "),
+        paste(genus, epithet, sep = " ")
+      ),
+      accepted_author = if_else(
+        infrasp %in% .infrasp_abb,
+        leftover,
+        paste(infrasp, infrasp_name, leftover, sep = " ")
+      )
+    ) %>% 
+    select(submitted_name, fuzzy, fuzzy_dist, status, accepted_id, accepted_name, accepted_author, refdata_id, refdata, matching_algo)
+  ## ---
+  
+  ## output object to .GlobalEnv but just to be safe, also write csv back to demo file
+  if (!is.null(.save_table)) {
+    write_csv(solved_out, paste0(.save_table, "/", filename, "-" , format(Sys.time(), format = "%Y-%m-%d-%H%M"), "-resLCVP.csv"))
+    write_tsv(tibble(NULL), paste0(.save_table, "/", filename, "-", format(Sys.time(), format = "%Y-%m-%d-%H%M"), "-resLCVP-", dt,"-secs.txt"))
+  }
+  
+  solved_out
+    
+}
+
+
+
+
+
---
+++ R/solve_wfo.R
@@ -0,0 +1,143 @@
+
+## Function to run a Taxonomic Name Resolution algorithm and output result harmonized with:
+## 1. The whole input list of species is returned
+## 2. Matches are returned with species and authors separated
+## 3. Taxonomic status in lower case
+## 4. Score or fuzzy matching indicator
+
+
+## --- World Flora Online ---------------------------------------------------
+## --- Offline
+## --- Data source: accept WFO reference data "http://104.198.143.165/files/WFO_Backbone/_WFOCompleteBackbone/WFO_Backbone.zip"
+## ---              or any dataset converted with WorldFlora::new.backbone()
+## --- Algorithm: WFO.match()
+## --- Performs better with parallel computing
+## ---
+## --- http://www.worldfloraonline.org/
+## --- https://github.com/cran/WorldFlora
+## ---
+## --- function params: 
+## ---    .data: vector of taxonomic names with or without authors. Genus are not evaluated if submitted alone. 
+## ---           Preferably output of species_clean().
+## ---    .save_table: NULL or path to export the results. if .path exists (function embedded 
+## ---                 in a higher level function call) it is used in the file name 
+
+solve_wfo <- function(.taxon, .ref_file, .ref_name, .multicore = TRUE, .save_table = NULL){
+  
+  ## !!! For testing only
+  # .path    <- "demo/NFMA_species_mess.csv"
+  # .taxon <- .path %>% species_clean() %>%
+  #   filter(!is.na(input_ready)) %>%
+  #   pull(input_ready) %>%
+  #   unique()
+  # .ref_file <- paste0(path_data, "/", wfo_backbone_lcvp)
+  # .ref_name <- "Leipzig Catalogue of Vascular Plants"
+  # .multicore <-  TRUE
+  # .save_table <- path_res
+  ## !!! 
+  
+  ## Check function inputs
+  stopifnot(is.logical(.multicore))
+  stopifnot(is.character(.taxon))
+  stopifnot(is.character(.ref_file))
+  stopifnot(is.character(.ref_name))
+  stopifnot(is.null(.save_table)|is.character(.save_table))
+
+  stopifnot("WorldFlora" %in% installed.packages())
+  
+  stopifnot(file.exists(.ref_file))
+  
+  if (.multicore) stopifnot("furrr" %in% installed.packages()) ## future and parallel are loaded from furrr
+  if (!is.null(.save_table)) stopifnot(dir.exists(.save_table))
+  
+  ## Remove genus alone from the data
+  input <- setdiff(.taxon, word(.taxon)) %>% unique() %>% sort()
+  
+  ## !!! For testing only
+  set.seed(12)
+  input <- sample(input, 50) %>% sort()
+  
+  ## Find table name if .path exists
+  filename     <- if_else(exists(".path"), get_filename(.path), "")
+  ref_filename <- if_else(str_detect(.ref_file, "classification.txt"), "WFO_backbone", get_filename(.ref_file))
+  
+  
+  ## --- RUN WFO ---
+  message(paste0("...Running WFO with ", ref_filename, " dataset."))
+  time1 <- Sys.time()
+  
+  if (.multicore) {
+    
+    ## Set nb workers
+    nb_cores <- ifelse(future::availableCores() <= 2, 1, future::availableCores() - 2)
+    
+    
+    ## Create function to avoid loading the whole environment to the workers
+    crt_wfo <- carrier::crate(
+      input     = input,
+      .ref_file = .ref_file,
+      function(.x){
+        message("Processing sequence: ", min(.x), " to ", max(.x), ".")
+        wfo_input = input[.x]
+        WorldFlora::WFO.match(wfo_input, WFO.file = .ref_file)
+      })
+    
+    ## Make chunks
+    input_chunks <-furrr:::make_chunks(n_x = length(input), n_workers = n_cores)
+    
+    ## Run crated function 
+    future::plan(multisession)
+    solved_wfo <- furrr::future_map_dfr(.x = input_chunks, .f = crt_wfo, .options = furrr::furrr_options(globals = FALSE))
+    future::plan(sequential)
+  
+  } else {
+    
+    solved_wfo <- WorldFlora::WFO.match(input, WFO.file = .ref_file)
+    
+  } ## End if .multicore
+  
+  time2 <- Sys.time()
+  dt    <- round(as.numeric(time2-time1, units = "secs"))
+  message(paste0("...Taxons solved with WFO", " - ", dt, " sec."))
+  ## --- END RUN WFO ---
+  
+  # table(solved_wfo$taxonomicStatus, useNA = "always")
+  # table(solved_wfo$Fuzzy, useNA = "always")
+  
+  ## --- Harmonize ---
+  solved_out <- tibble(submitted_name = .taxon) %>%
+    left_join(solved_wfo, by = c("submitted_name" = "spec.name")) %>%
+    mutate(
+      fuzzy_dist      = if_else(is.na(Fuzzy.dist), 0, Fuzzy.dist),
+      fuzzy           = Fuzzy,
+      status          = if_else(taxonomicStatus == "", "noref", taxonomicStatus),
+      accepted_id     = acceptedNameUsageID,
+      refdata_id      = ref_filename,
+      refdata         = .ref_name,
+      matching_algo   = "WorldFlora::WFO.match()",
+      accepted_name   = scientificName,
+      accepted_author = scientificNameAuthorship,
+    ) %>% 
+    select(submitted_name, fuzzy, fuzzy_dist, status, accepted_id, accepted_name, accepted_author, refdata_id, refdata, matching_algo)
+  ## ---
+  
+  ## output object to .GlobalEnv but just to be safe, also write csv back to demo file
+  if (!is.null(.save_table)) {
+    write_csv(solved_out, 
+              paste0(.save_table, "/", filename, "-" , 
+                     format(Sys.time(), format = "%Y-%m-%d-%H%M"), 
+                     "-resWFO-with", ref_filename, ".csv"))
+    write_tsv(tibble(NULL), 
+              paste0(.save_table, "/", filename, "-", 
+                     format(Sys.time(), format = "%Y-%m-%d-%H%M"), 
+                     "-resWFO-with", ref_filename, "-", dt,"-secs.txt"))
+  }
+  
+  solved_out
+    
+}
+
+
+
+
+
