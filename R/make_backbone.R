f0c9d9d7d40412efbd445c787e7b9ac9cd1c79d0
gael.sola <gaelsola@hotmail.fr>
2021-09-06 02:17:05 +0700

    not finished, making iucn_checklist

--- R/make_backbone.R
+++ R/make_backbone.R
@@ -236,9 +236,9 @@ if (!(wfo_backbone_gbif %in% list.files(recursive = T))) {
 
 
 
-## --- 4. WFO backbone from UICN --------------------------------------------
+## --- 4. IUCN check list ---------------------------------------------------
 
-## UICN is more restricted than other services. We might need to guide users to create an account and download manualy:
+## IUCN is more restricted than other services. We might need to guide users to create an account and download manually:
 ## See cached data from taxadb: https://github.com/boettiger-lab/taxadb-cache/blob/master/R/iucn.R
 ## Directions:
 ## - Go to: https://www.iucnredlist.org/
@@ -248,43 +248,115 @@ if (!(wfo_backbone_gbif %in% list.files(recursive = T))) {
 ##    + Type: species
 ##    + Taxonomy: filter  Plantae > Tracheophyta
 ##    + include Species, Subspecies and varieties, Subpopulations
-## - Download search summary. fill path in global.R Admin input section
+## - Download search summary. 
+## - Find zip when file.choose() called
 
 
-# if (!(wfo_backbone_uicn %in% list.files(recursive = T))) {
-# 
-#   message("Creating WFO backbone dataset from UICN with taxadb::td_create()...")
-#   time1 <- Sys.time()
-# 
-#   ## Download a local copy of the data if necessary
-#   taxadb::td_create("uicn", version = "2020", overwrite = F)
-# 
-#   ## Check status
-#   taxa_tbl("uicn", version = "2020") %>% pull(taxonomicStatus) %>% unique()
-# 
-#   ## Make backbone
-#   gbif_tab <- taxa_tbl("gbif", version = "2020") %>%
-#     filter(kingdom == "Plantae") %>%
-#     as_tibble() %>%
-#     arrange() %>%
-#     WorldFlora::new.backbone(
-#       taxonID = "taxonID",
-#       scientificName = "scientificName",
-#       scientificNameAuthorship = "scientificNameAuthorship",
-#       acceptedNameUsageID =  "acceptedNameUsageID",
-#       taxonomicStatus = "taxonomicStatus"
-#     )
-# 
-#   data.table::fwrite(gbif_tab, file = wfo_backbone_gbif, sep = "\t")
-# 
-#   ## !!! Remove tmp objects
-#   rm(gbif_tab)
-#   ## !!!
-# 
-#   time2 <- Sys.time()
-#   dt <- round(as.numeric(time2-time1, units = "secs"))
-#   message(paste0("...Done", " - ", dt, " sec."))
-# 
-# } ## End if wfo_backbone_uicn
+if (!(iucn_checklist %in% list.files(recursive = T))) {
+
+  message("Creating IUCN checklist from manual download...")
+  time1 <- Sys.time()
+
+  ## Path to IUCN Manual download
+  iucn_download <- file.choose() ## "C:\\Users\\Admin\\Downloads\\redlist_species_data_e73c2f06-3056-4897-9fb3-824f5b326757.zip"
+  
+  ## Unzip files to tempdir()
+  unzip(iucn_download, exdir = file.path(tempdir(), "iucn"), files = c("taxonomy.csv", "synonyms.csv", "simple_summary.csv"))
+  
+  list.files(file.path(tempdir(), "iucn"))
+  #unlink(file.path(tempdir(), "iucn"), recursive = T)
+  
+  ## Read files
+  iucn        <- list.files(file.path(tempdir(), "iucn"), full.names = TRUE) %>% map(read_csv)
+  names(iucn) <- list.files(file.path(tempdir(), "iucn")) %>% str_remove(".csv")
+  
+  ## Checks 
+  table(iucn$synonyms$infraType)
+  table(iucn$simple_summary$redlistCategory)
+  
+  ## Red list codes: ND added by Lauri, no data (!= not evaluated)
+  iucn_codes <- tibble(
+    iucn_code  = c("ND", "NE", "DD", "LC", "NT", "VU", "EN", "CR", "EW", "EX"), 
+    iucn_label = c("Not in Red List", "Not Evaluated", "Data Deficient", "Least Concern", 
+                   "Near Threatened", "Vulnerable",  "Endangered", "Critically Endangered", 
+                   "Extinct in the Wild", "Extinct")
+    )
+  
+  iucn_conv <- tibble(
+    iucn_label = c("Lower Risk/conservation dependent", "Lower Risk/least concern", "Lower Risk/near threatened"),
+    iucn_code = c("NT", "LC", "NT")
+  )
+  
+  ## Simplify summary
+  iucn_redlist <- iucn$simple_summary %>%
+    mutate(id = as.character(internalTaxonId)) %>%
+    select(id, iucn_label = redlistCategory) %>%
+    distinct()
+  
+  ## Remove authors
+  iucn_syn <- iucn$synonyms %>%
+    mutate(
+      id_accepted     = as.character(internalTaxonId),
+      name_cor        = name            %>% str_remove(" \\[orth\\. error\\]| \\{orth\\. error\\]"),
+      author_cor      = speciesAuthor   %>% str_remove(" \\[orth\\. error\\]| \\{orth\\. error\\]"),
+      infauthor_cor   = infrarankAuthor %>% str_remove(" \\[orth\\. error\\]| \\{orth\\. error\\]"),
+      author_regex    = author_cor    %>% str_replace_all("\\.", "\\\\.") %>% str_replace_all("\\(", "\\\\(") %>% str_replace_all("\\)", "\\\\)"),
+      infauthor_regex = infauthor_cor %>% str_replace_all("\\.", "\\\\.") %>% str_replace_all("\\(", "\\\\(") %>% str_replace_all("\\)", "\\\\)"),
+      synonym_name   = if_else(
+        is.na(infauthor_cor), 
+        name_cor %>% str_remove(paste0(" ", author_regex)),
+        name_cor %>% str_remove(paste0(" ", author_regex)) %>% str_remove(paste0(" ", infauthor_regex))
+        ),
+      synonym_author = if_else(is.na(infauthor_cor), author_cor, infauthor_cor)
+      ) %>%
+    select(id, synonym_name, synonym_author) %>%
+    distinct()
+  
+  ## Add IUCN red list category and synonyms to taxonomy
+  iucn_taxo1 <- iucn$taxonomy %>%
+    mutate(id = as.character(internalTaxonId)) %>%
+    select(id, sc_name = scientificName, author = authority) %>%
+    left_join(iucn_redlist, by = "id") %>%
+    left_join(iucn_syn, by = "id") %>%
+    left_join(bind_rows(iucn_codes, iucn_conv), by = "iucn_label") %>%
+    left_join(iucn_codes, by = "iucn_code", suffix = c("", "_cor")) %>% ## Update Lower Risk to post 2001 categories
+    distinct() %>%
+    arrange(sc_name)
+  
+  iucn_taxo2 <- iucn_taxo1 %>% 
+    select(id, sc_name = accepted_name, author = accepted_author, iucn_code, iucn_label_cor) %>%
+    filter(!is.na(sc_name)) %>%
+    distinct() %>%
+    mutate(id = paste0(id, "_acc")) %>%
+    bind_rows(iucn_taxo, .) %>%
+    select(-iucn_label)
+  
+  ## Count entries
+  num_taxon <- iucn_taxo2 %>% 
+    group_by(sc_name) %>% summarise(count_taxon = n())
+    
+  iucn_taxo3 <- iucn_taxo2 %>%
+    left_join(num_taxon, by = "sc_name") %>%
+    mutate(
+      accepted_name   = if_else(count_taxon > 1, NA_character_, accepted_name),
+      accepted_author = if_else(count_taxon > 1, NA_character_, accepted_author), 
+      status          = case_when(
+        is.na(accepted_name) & count_taxon == 1 ~ "accepted",
+        is.na(accepted_name) & count_taxon  > 1 ~ "unresolved",
+        !is.na(accepted_name)                   ~ "synonym",
+        TRUE ~ NA_character_
+      )
+    ) %>%
+    distinct()
+  
+  ## !!! Remove tmp objects
+  rm(gbif_tab)
+  ## !!!
+
+  time2 <- Sys.time()
+  dt <- round(as.numeric(time2-time1, units = "secs"))
+  message(paste0("...Done", " - ", dt, " sec."))
+
+} ## End if wfo_backbone_uicn
 
 
