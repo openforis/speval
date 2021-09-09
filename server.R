891094745064398bbc39940677915c2f4542f3c6
gael.sola <gaelsola@hotmail.fr>
2021-09-10 01:45:04 +0700

    updated app to multi tabs and automatic reveal panels

--- server.R
+++ server.R
@@ -1,13 +1,10 @@
 ######################################################################
 # Species Validation Tool                                        #####
 # SPECIES NAMES VALIDATION TOOL                                        
-# (c) Lauri Vesa, Javier Garcia-Perez, Elisee Tchana, Gael Sola FAO    
+# (c) Lauri Vesa, Javier Garcia-Perez, Elisee Tchana, Gael Sola. FAO    
 # server.R file                                                        
-# updated 17.8.2021
+# updated 8.9.2021
 ######################################################################
-
-
-
 
 
 ################
@@ -16,7 +13,10 @@
 
 shinyServer(function(input, output, session) {
   
-  ## Initial Disclaimer ###################################################
+  
+  
+  ## Initial Disclaimer #####################################################
+  
   sendSweetAlert(
     session = session,
     title =  NULL,
@@ -34,173 +34,147 @@ shinyServer(function(input, output, session) {
   )
   
   
-  output$contents <- renderTable({
-    file <- input$file1
-    ext <- tools::file_ext(file$datapath)
+  
+  ## Turn off conditionalPanel for valdiiation and results ##################
+  
+  ## Initiate condition to show the validation panel and the results
+  output$file_valid <- reactive({ FALSE })
+  outputOptions(output, "file_valid", suspendWhenHidden = FALSE)
+  
+  output$validation_done <- reactive({ FALSE })
+  outputOptions(output, "validation_done", suspendWhenHidden = FALSE)
+  
+  
+  
+  ## Read and show species list #############################################
+  
+  check_table <- reactive({
+    req(input$file1)
     
-    req(file)
+    ext <- tools::file_ext(input$file1$name)
+    
     validate(need(ext == "csv", "Please upload a csv file"))
-    
-    read.csv(file$datapath, header = TRUE) #  input$header)
-
+    read.csv(input$file1$datapath, header = TRUE) #  input$header)
   })
   
-  output$text <- renderText("Before you can import a species list, you need to prepare a table and save it as a CSV format file. Then upload the file into this app.")
+  output$contents <- renderTable( check_table() )
+  #output$contents <- renderTable(input$file1)
   
-  # observe({ 
-  #   file.copy(input$file1$datapath,
-  #             input$file1$name, overwrite = TRUE)
-  # })
-  
-  disable("run_analyse")
-  
+  # If file is valid show indicator panels and buttons
   observeEvent(input$file1, {
-    # file     <- input$file1
-    # stable   <- read.csv(file$datapath, header = TRUE)
-    enable("run_analyse")
-    shinyjs::html(id = "console_text", "")
-    # shinyjs::html(id = "console_text", html = paste0("Number of names: ", nrow(stable), '<br>'), add = TRUE)
-    output$stat1           <- renderTable(NULL)
-    output$stat2           <- renderTable(NULL)
-    output$stat3           <- renderTable(NULL)
-    output$result_contents <- renderTable(NULL)
-    
-  })
+    if (tools::file_ext(input$file1$name) == "csv") {
+      output$file_valid <- reactive({ TRUE })
+    } else {
+      output$file_valid <- reactive({ FALSE })
+    }
+  }) ## End observeEvent
   
   
-  ## Run the solving meta-function
+  
+  ## Run the validation master function #####################################
+  
   species_solved <- eventReactive(input$run_analyse, {
     
+    if ("scientific_name" %in% names(check_table())) {
+      
       withCallingHandlers({
-        shinyjs::html("console_text", "")
-        #source("Species_search_17_August_2021.R", local = TRUE)
         species_solve(
-          .path       = input$file1$datapath, 
-          .how_to     = "compare", 
-          .with_lcvp  = FALSE, 
-          .save_table = NULL, 
-          .multicore  = TRUE, 
+          .path       = input$file1$datapath,
+          .how_to     = "compare",
+          .with_lcvp  = FALSE,
+          .save_table = NULL,
+          .multicore  = input$opt_multicore,
           .ref_lcvp   = wfo_backbone_lcvp,
           .ref_wfo    = wfo_file,
           .tx_src     = src_tropicos,
-          .ref_ncbi   = wfo_backbone_ncbi, 
-          .ref_gbif   = wfo_backbone_gbif, 
+          .ref_ncbi   = wfo_backbone_ncbi,
+          .ref_gbif   = wfo_backbone_gbif,
           .ref_gts    = gts_file,
           .ref_iucn   = iucn_checklist
-          )
+        )
       },
       message = function(m) {
         shinyjs::html(id = "console_text", html = paste0(m$message, '<br>'), add = TRUE)
       })
-    
+      
+    } else {
+  
+      shinyjs::html(id = "console_text", html = "The input file doesn't have 'scientific_name' column. <br>")
+      shinyjs::html(id = "console_text", html = "Please re-upload a CSV file with the species list under a column named: 'scientific_name'. <br>", add = TRUE)
+      list(valid = FALSE)
+      
+    } ## End if
+
   }) ## End eventReactive
   
   
-  ## Show outputs
+  
+  ## Activate conditionalPanel if species validation done ###################
+  
   observeEvent(input$run_analyse, {
     
+    if (species_solved()$valid) {
+      output$validation_done <- reactive({ TRUE })
+      shinyjs::show("after_valid")
+    } else {
+      output$validation_done <- reactive({ FALSE })
+      shinyjs::hide("after_valid")
+    }
+    
+  }) ## End observeEvent
+    
+  ## Change tab
+  observeEvent(
+    input$to_results,
+    updateTabItems(session, "tabs", "results")
+  )
+  
+  
+  
+  ## Prepare outputs ########################################################
+  
+  observeEvent(species_solved()$valid, {
+
     ## Species solved table
     output$result_contents <- renderTable({
       species_solved()$species_final
     })
-    
+
     output$downloadData <- downloadHandler(
       filename <- function() { paste0(input$file1$name %>% str_remove(".csv"), "-species.csv") },
       content  <- function(file) { readr::write_csv(species_solved()$species_final, file) }
       )
-    
+
     ## Stat1
     output$stat1 <- renderTable({
       species_solved()$stat1
     })
-    
+
     output$downloadStat1 <- downloadHandler(
       filename <- function() { paste0(input$file1$name %>% str_remove(".csv"),"-stat1.csv") },
       content  <- function(file) { readr::write_csv(species_solved()$stat1, file) }
     )
-    
+
     ## Stat2
     output$stat2 <- renderTable({
       species_solved()$stat2
     })
-    
+
     output$downloadStat2 <- downloadHandler(
       filename <- function() { paste0(input$file1$name %>% str_remove(".csv"), "-stat2.csv") },
       content  <- function(file) { readr::write_csv(species_solved()$stat2, file) }
     )
-    
+
     ## Stat3
     output$stat3 <- renderTable({
       species_solved()$stat3
     })
-    
+
     output$downloadStat3 <- downloadHandler(
       filename <- function() { paste0(input$file1$name %>% str_remove(".csv"), "-stat3.csv") },
       content  <- function(file) { readr::write_csv(species_solved()$stat3, file) }
     )
-    
+
   }) ## End observeEvent
   
-  
-  # observeEvent(input$run_analyse, {
-  #   withCallingHandlers({
-  #     shinyjs::html("console_text", "")
-  #     #source("Species_search_17_August_2021.R", local = TRUE)
-  #     
-  #   },
-  #   message = function(m) {
-  #     shinyjs::html(id = "console_text", html = paste0(m$message, '<br>'), add = TRUE)
-  #   })
-  #   
-  #   if(file.exists(outFile1)){
-  #     output$result_contents <- renderTable({
-  #       
-  #       read.csv(outFile1)
-  #     }, na = "" )
-  #     
-  #     output$downloadData <- downloadHandler(
-  #       filename <- function() {
-  #         paste(outFile1, sep='')
-  #       },
-  #       content <- function(file) {
-  #         file.copy(outFile1, file)
-  #       }
-  #     )
-  #   }
-  #   
-  #   if(file.exists(outFile2)){
-  #     output$result_statistics <- renderTable({
-  #       
-  #       
-  #       read.csv(outFile2)
-  #     })
-  #     
-  #     output$downloadData2 <- downloadHandler(
-  #       filename <- function() {
-  #         paste(outFile2, sep='')
-  #       },
-  #       content <- function(file) {
-  #         file.copy(outFile2, file)
-  #       }
-  #     )
-  #   }
-  #   
-  #   if(file.exists(outFile3)){
-  #     output$result_statistics_2 <- renderTable({
-  #       
-  #       read.csv(outFile3, header = TRUE)
-  #     })
-  #     
-  #     output$downloadData3 <- downloadHandler(
-  #       filename <- function() {
-  #         paste(outFile3, sep='')
-  #       },
-  #       content <- function(file) {
-  #         file.copy(outFile3, file)
-  #       }
-  #     )
-  #   }
-  #   
-  # })
-
 })
--- ui.R
+++ ui.R
@@ -1,9 +1,9 @@
-######################################################################
-# Species Validation Tool                                    #########
-# SPECIES NAMES VALIDATION TOOL                              #########
-# (c) Lauri Vesa, Javier Garcia-Perez, Elisee Tchana, FAO    #########
-# ui.R file                                                  #########
-######################################################################
+#################################################################################
+# Species Validation Tool                                               #########
+# SPECIES NAMES VALIDATION TOOL                                         #########
+# (c) Lauri Vesa, Geal Sola, Javier Garcia-Perez, Elisee Tchana, FAO    #########
+# ui.R file                                                             #########
+#################################################################################
 
 
 ###########
@@ -34,14 +34,16 @@ shinyUI(fluidPage(
     
     dashboardSidebar(width = 300,
       sidebarMenu(
+        id = "tabs",
         HTML(paste0(
           "<br>",
           "<img style = 'display: block; margin-left: auto; margin-right: auto;' src='Open-foris-Logo160.jpg' width = '186'>",
           "<br>"
         )),
-        menuItem("Home", tabName = "home", icon = icon("home")),
-        menuItem("Species Validation", tabName = "table", icon = icon("table")),
-        menuItem("Releases", tabName = "releases", icon = icon("tasks")),
+        menuItem("Home"          , tabName = "home"    , icon = icon("home")),
+        menuItem("Run Validation", tabName = "valid"   , icon = icon("leaf")),
+        menuItem("Get results"   , tabName = "results" , icon = icon("table")),
+        menuItem("Releases"      , tabName = "releases", icon = icon("tasks")),
         HTML(paste0(
           "<br><br><br><br><br><br><br><br><br>",
           "<br><br><br><br><br><br><br><br><br>",
@@ -51,9 +53,7 @@ shinyUI(fluidPage(
               
               "<td style='padding: 5px;'><a href='https://www.youtube.com/channel/UCKwDbo_cf5PhwHDGOIcNA2A' target='_blank'><i class='fab fa-youtube fa-lg'></i></a></td>",
               "<td style='padding: 5px;'><a href='https://twitter.com/OpenForis?s=20' target='_blank'><i class='fab fa-twitter fa-lg'></i></a></td>",
-          
-    
-              
+
           "</table>",
           "<br>"),
         HTML(paste0(
@@ -70,103 +70,181 @@ shinyUI(fluidPage(
      
       tabItems(
         
-        tabItem(tabName = "home",
-          
-          # home section
-          includeMarkdown("www/home.md")
-          
-        ),
+        ## --- Home ---------------------------------------------------------
+        tabItem(tabName = "home", includeMarkdown("www/home.md")),
         
-
         
+        
+        ## --- Run validation ----------------------------------------------- 
         tabItem(
-          tabName = "table",
+          tabName = "valid",
           
-          sidebarPanel(
-
-            fileInput("file1", HTML("Upload CSV File <br><i> Input file with one or two columns: (code,) scientific_name</i>"),
-                      accept = c(
-                        "text/csv",
-                        "text/comma-separated-values,text/plain",
-                        ".csv")
+          #fluidRow(
+            
+            ## Title
+            h2("Upload species list and run the species validation algorithm"),
+            br(), 
+            h4("Before you can import a species list, 
+               you need to prepare a table and save it as a CSV format file. 
+               Then upload the file into this app."),
+            br(), br(),
+            
+            ## Left column, load data 
+            column(
+              6,
+              h4(strong("Upload CSV file")),
+              br(),
+              h4(em("Input file with one or two columns: ")),
+              h4(em("code (optional), scientific_name")),
+              fileInput("file1", NULL, accept = c(".csv", ".tsv")),
+              #br(),
+              fluidRow(
+                tableOutput("contents"), 
+                style = 'height:500px; width:100%; background-color: #FFFFFF; 
+                 margin-bottom: 30px; padding: 25px; overflow-y: scroll'
+                ),
+              br()
             ),
-#            checkboxInput("header", "Header", TRUE),
-        
+            
+            ## Right column, run validation
+            column(
+              6,
+              h4(strong("Run the validation algorithm")),
+              br(),
+              conditionalPanel(
+                condition = "output.file_valid",
 
-            fluidRow(
-              box(
-                "APPLIED REPOSITORIES", br(), br(),
-                "* Leipzig Catalogue of Vascular Plants (LCVP)", br(),
-                "* Tropicos - Missouri Botanical Garden", br(),
-                "* Plants of the World Online (Kew)", br(),
-                "* National Center for Biotechnology Information (NBCI)", br(),
-                "* World Flora Online (WFO)", br(),
-                "* Global Biodiversity Information Facility (GBIF)", br(),
-                "* Global Tree Search (GTS)", br(), 
+                checkboxInput("opt_multicore", "Multicore processing", TRUE ),
+                actionButton("run_analyse", "Run validation", class = "btn-primary"),
+                br(), br(), br(), br(),
+                fluidRow(
+                  tableOutput("console_text"), 
+                  style = 'height:400px; witdh=100%; background-color: #000000; 
+                  color: #FFFFFF; margin-bottom: 30px; padding: 25px; overflow-y: scroll'
+                ),
+                hidden(
+                  div(
+                    id = "after_valid",
+                    h4(icon("arrow-right"), "Continue to results"),
+                    HTML("&nbsp;"),
+                    actionButton("to_results", "Results")
+                  )
+                )
                 
-                width = 12,
-              )),
-            tags$br(),
-            actionButton("run_analyse", "Run validation", class = "btn-primary")
-          ),
-
-          mainPanel(
-            tabsetPanel(
-              tabPanel("About input data",
-                       tags$br(),
-                       
-
-                       textOutput("text"),
-                       hr(),
-                       tags$br(),
-                       div(style='height:300px; width:100%; overflow: scroll ;background: #000000; color: #FFFFFF; margin-bottom: 30px; padding: 25px',
-                           tableOutput("console_text")
-                       ),
-
-                    
-              ),
-              tabPanel("Input data",
-                       tags$br(),
-                       div(style='height:650px; width:100%; background-color: #FFFFFF; overflow-y: scroll',
-                           tableOutput("contents")
-                       )
-                       
-              ),
-              tabPanel("Result table",
-                       tags$br(),
-                       downloadButton('downloadData', 'Download data'),
-                       
-                       div(style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll',
-                           tableOutput("result_contents")
-                       )
-                       
-
-              ),
-              tabPanel("Statistics",
-                       tags$br(),
-                       downloadButton('downloadStat1', 'Download stat1'),
-                       downloadButton('downloadStat2', 'Download stat2'),
-                       downloadButton('downloadStat3', 'Download stat3'),
-                       div(style='height:650px; width:100%; background-color: #FFFFFF; overflow: scroll',
-                           tableOutput("stat1"),
-                           tableOutput("stat2"),
-                           tableOutput("stat3"),
-                       ),
-                     
-              ),
-              id="tabs"), width = 7,  # tabsetPanel
-          )# mainPanel
-        
+              ) ## End conditionalPanel
+            )
+          #) ## End fluidRow
           
         ),
         
         
+        
+        ## --- Get results --------------------------------------------------
+        tabItem(
+          tabName = "results",
+          
+          ## Title
+          h2("Species validation results"),
+          br(), 
+          
+          conditionalPanel(
+            condition = "output.validation_done",
+            
+            tabsetPanel(
+              tabPanel("Main results",
+                       tabName = "res_table1",
+                       tags$br(),
+                       downloadButton('downloadData', 'Download'),
+                       fluidRow(tableOutput("result_contents"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+              ),
+              tabPanel("Stat1",
+                       tabName = "res_stat1",
+                       tags$br(),
+                       downloadButton('downloadStat1', 'Download'),
+                       fluidRow(tableOutput("stat1"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+              ),
+              tabPanel("Stat2",
+                       tabName = "res_stat2",
+                       tags$br(),
+                       downloadButton('downloadStat2', 'Download'),
+                       fluidRow(tableOutput("stat2"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+              ),
+              tabPanel("Stat3",
+                       tabName = "res_stat3",
+                       tags$br(),
+                       downloadButton('downloadStat3', 'Download'),
+                       fluidRow(tableOutput("stat3"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+              )
+            ) ## End tabsetPanel
+          ) ## End conditionalPanel
+        ),
+          
+        #   mainPanel(
+        #     tabsetPanel(
+        #       tabPanel("Upload and run",
+        #                tags$br(),
+        #                fileInput("file1", 
+        #                          HTML("Upload CSV File <br><i> Input file with one or two columns: (code,) scientific_name</i>"),
+        #                          accept = ".csv"
+        #                          # accept = c(
+        #                          #   "text/csv",
+        #                          #   "text/comma-separated-values,text/plain",
+        #                          #   ".csv")
+        #                ),
+        #                fluidRow(
+        #                  column(width = 12,
+        #                         textOutput("text")
+        #                  )),
+        #                checkboxInput("opt_multicore", "Multicore processing", TRUE ),
+        #                actionButton("run_analyse", "Run validation", class = "btn-primary"),
+        #                
+        #                tags$br(),tags$br(),
+        #                fluidRow(tableOutput("console_text"), style = "height:550px; background-color: #000000; color: #FFFFFF; margin-bottom: 30px; padding: 25px; overflow-y: scroll")
+        #       ),
+        #       tabPanel("Input data",
+        #                tags$br(),
+        #                fluidRow(tableOutput("contents"), style='height:650px; width:100%; background-color: #FFFFFF; overflow-y: scroll')
+        #       ),
+        #       tabPanel("Results",
+        #                tabsetPanel(
+        #                   tabPanel("Main results",
+        #                     tabName = "res_table1",
+        #                     tags$br(),
+        #                     downloadButton('downloadData', 'Download'),
+        #                     fluidRow(tableOutput("result_contents"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+        #                   ),
+        #                   tabPanel("Stat1",
+        #                            tabName = "res_stat1",
+        #                            tags$br(),
+        #                            downloadButton('downloadStat1', 'Download'),
+        #                            fluidRow(tableOutput("stat1"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+        #                   ),
+        #                   tabPanel("Stat2",
+        #                            tabName = "res_stat2",
+        #                            tags$br(),
+        #                            downloadButton('downloadStat2', 'Download'),
+        #                            fluidRow(tableOutput("stat2"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+        #                   ),
+        #                   tabPanel("Stat3",
+        #                            tabName = "res_stat3",
+        #                            tags$br(),
+        #                            downloadButton('downloadStat3', 'Download'),
+        #                            fluidRow(tableOutput("stat3"), style='height:650px; width:100%; background-color: #FFFFFF;overflow: scroll')
+        #                   )
+        #                   
+        #                )
+        #       ), id="tabs"), width = 12,  # tabsetPanel
+        #   )# mainPanel
+        # ),
+        
+        
+        ## --- Releases ----------------------------------------------------- 
         tabItem(tabName = "releases", includeMarkdown("www/releases.md"))
               
-      )
+      ) ## End tabItems
     
     ) # end dashboardBody
   
-  )# end dashboardPage
+  ) # end dashboardPage
 
 ))
